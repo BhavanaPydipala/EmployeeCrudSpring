e18709323be389e6920784acb529d45db77dc63c
Bhavana Pydipala <pydipalabhavana07@gmail.com>
2020-04-09 17:13:10 +0530

    commit

---
+++ SpringBoot-Exclusions/src/main/java/com/cap/anurag/dao/EmployeeDao.java
@@ -0,0 +1,20 @@
+package com.cap.anurag.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.cap.anurag.entity.Employee;
+
+public interface EmployeeDao {
+	
+	public void save(Employee emp) ;
+	
+	public Employee findById(Integer empId);
+	
+	public Employee updateEmp(Employee emp);
+	
+	public Employee deleteEmp(Integer empId);
+	
+	List<Employee> findAllEmployees();
+	
+}
